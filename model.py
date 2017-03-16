1de9ff220f0287a0852df39262e8de896464a4e5
Nabeel Mukhtar <nabeelmukhtar@yahoo.com>
2017-03-16 19:05:36 +0500

    First model from lectures.

---
+++ model.py
@@ -0,0 +1,53 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+        
+images = []
+measurements = []
+
+for line in lines[1:]:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+augmented_images = []
+augmented_measurements = []
+
+for image,measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1.0)
+    
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+from keras.models import Sequential
+from keras.layers.core import Dense, Flatten, Lambda, Cropping2D
+from keras.layers.convolution import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+
+model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation='relu'))
+model.add(Flatten())
+model.add(Dense(120))
+
+model.compile(loss='mse', optimizer = 'adam')
+
+model.fit(X_train, y_train, nb_epoch=3, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
