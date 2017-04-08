39c318a2e2a6789c1f383ccdf50af25fee617bb2
Nabeel Mukhtar <nabeelmukhtar@yahoo.com>
2017-04-08 15:54:08 +0500

    Going deeper.

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import sklearn
 import numpy as np
-from random import shuffle
+from random import random, shuffle
 
 def augment_brightness(image, steer):
     image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
@@ -14,7 +14,7 @@ def augment_brightness(image, steer):
     image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
     return image1, steer
     
-def translate_image(image,steer,trans_range = 100):
+def translate_image(image, steer, trans_range = 100):
     rows,cols,_ = image.shape
     # Translation
     tr_x = trans_range*np.random.uniform()-trans_range/2
@@ -31,6 +31,9 @@ def flip_image(image, steer):
     steer_flip = -1.0 * steer
     
     return image_flip, steer_flip
+
+def copy_image(image, steer):
+    return image, steer
 
 def increase_contrast(image, steer):
     img_yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
@@ -52,18 +55,18 @@ with open('./data/driving_log.csv') as csvfile:
         
 from sklearn.model_selection import train_test_split
 
-augmentation_funcs = [flip_image, translate_image]
+augmentations = [copy_image, flip_image, translate_image, augment_brightness, increase_contrast]
 
-samples_per_line = 3 + (3 * len(augmentation_funcs))
+samples_per_line = 3 * len(augmentations)
 steering_correction = 0.3
 steering_aggressivity = 1.0
 steering_min = 0.05
 steering_keep_prob = 0.8
 
 # remove images where steering is greater than 0.8
-filtered_samples = [x for x in lines[1:] if float(x[3]) <= 1.0 / steering_aggressivity]
+filtered_samples = [x for x in lines[1:] if abs(float(x[3])) <= 1.0 / steering_aggressivity]
 # remove images where steering is close to 0.
-filtered_samples = [x for x in filtered_samples if float(x[3]) > steering_min or np.random.uniform() < steering_keep_prob]
+filtered_samples = [x for x in filtered_samples if abs(float(x[3])) > steering_min or np.random.uniform() < steering_keep_prob]
 
 train_samples, validation_samples = train_test_split(filtered_samples, test_size=0.2)
 
@@ -77,32 +80,19 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                steering_center = float(batch_sample[3])
-                steering_left = steering_center + steering_correction
-                steering_right = steering_center - steering_correction
-    
-                center_image = cv2.imread('./data/IMG/' + batch_sample[0].split('/')[-1])
-                images.append(center_image)
-                angles.append(steering_center)
+                steering = float(batch_sample[3])
+                image_index = np.random.randint(3)
+                image = cv2.imread('./data/IMG/' + batch_sample[image_index].split('/')[-1])
+                if image_index == 1:
+                    steering = steering + steering_correction
+                elif image_index == 2:
+                    steering = steering - steering_correction
+                    
+                augment = random.choice(augmentations)
+                augmented_image, augmented_steer = augment(image, steering)
                 
-                left_image = cv2.imread('./data/IMG/' + batch_sample[1].split('/')[-1])
-                images.append(left_image)
-                angles.append(steering_left)
-
-                right_image = cv2.imread('./data/IMG/' + batch_sample[2].split('/')[-1])
-                images.append(right_image)
-                angles.append(steering_right)
-                
-                for augment in augmentation_funcs:
-                    augmented_image, augmented_steering = augment(center_image, steering_center)
-                    images.append(augmented_image)
-                    angles.append(augmented_steering)
-                    augmented_image, augmented_steering = augment(left_image, steering_left)
-                    images.append(augmented_image)
-                    angles.append(augmented_steering)
-                    augmented_image, augmented_steering = augment(right_image, steering_right)
-                    images.append(augmented_image)
-                    angles.append(augmented_steering)
+                images.append(augmented_image)
+                angles.append(augmented_steer)
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -110,8 +100,8 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+train_generator = generator(train_samples, batch_size=256)
+validation_generator = generator(validation_samples, batch_size=256)
 
 
 from keras.models import Sequential
@@ -126,25 +116,22 @@ model.add(Lambda(lambda x : x / 255.0 - 0.5, input_shape=(160,320,3)))
 
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(32, 3, 3, activation='elu'))
-model.add(Convolution2D(32, 3, 3, activation='elu'))
 model.add(MaxPooling2D())
 model.add(Dropout(0.5))
 
 model.add(Convolution2D(64, 3, 3, activation='elu'))
-model.add(Convolution2D(64, 3, 3, activation='elu'))
 model.add(MaxPooling2D())
 model.add(Dropout(0.5))
 
-# model.add(Convolution2D(128, 3, 3, activation='elu'))
-# model.add(Convolution2D(128, 3, 3, activation='elu'))
-# model.add(MaxPooling2D())
-# model.add(Dropout(0.5))
+model.add(Convolution2D(128, 3, 3, activation='elu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
 
 model.add(Flatten())
 
-# model.add(Dense(128))
-# model.add(Activation('elu'))
-# model.add(Dropout(0.5))
+model.add(Dense(128))
+model.add(Activation('elu'))
+model.add(Dropout(0.5))
 
 model.add(Dense(64))
 model.add(Activation('elu'))
